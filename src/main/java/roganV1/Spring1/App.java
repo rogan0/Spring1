76f1c68389e7c2cd7ce3b16ec89744920206d092
rogan <rogan@PengDuo>
2015-06-24 09:54:55 +0800

    first commit

---
+++ src/main/java/roganV1/Spring1/App.java
@@ -0,0 +1,13 @@
+package roganV1.Spring1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
